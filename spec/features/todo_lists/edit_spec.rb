8a439151db327efa35f8e1f7f7d058cde07ccfaa
Libby Schumacher-Knight <apprentice@StMac5-2.local>
2015-06-17 08:46:55 +1200

    add edit spec file

---
+++ spec/features/todo_lists/edit_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe 'editing todo lists' do
+  it "updates a todo list successfully with correct information" do
+    visit '/todo_lists'
+
+  end
+
+
+
+
+end
